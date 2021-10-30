af8356a4d2d823335db4959016af3d9f03a7bedb
santidalmasso <santidalmassoj@gmail.com>
2021-10-29 21:02:34 -0300

    Fix pwa

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -2,30 +2,34 @@ import { NextApiHandler } from "next";
 import prisma from "../../../prisma/prisma-client";
 import { compare } from "bcrypt";
 
-const credentialsAuth: NextApiHandler<any> = async (req, res) => {
-  if (req.method !== "POST") {
-    res.status(405).end();
+const credentialsAuth = async (req, res) => {
+  try {
+    if (req.method !== "POST") {
+      res.status(405).end();
+    }
+
+    const user = await prisma.user.findFirst({
+      where: { email: req.body.email },
+    });
+
+    if (!user) return res.status(400).end();
+
+    const arePasswordEqual = await compare(req.body.password, user.password);
+
+    if (arePasswordEqual) {
+      const tinyUser = {
+        name: user.name,
+        surname: user.surname,
+        email: user.email,
+      };
+
+      return res.status(200).json(tinyUser);
+    }
+
+    res.status(401).end();
+  } catch (err) {
+    res.status(500).json({ error: "failed to load data" });
   }
-
-  const user = await prisma.user.findFirst({
-    where: { email: req.body.email },
-  });
-
-  if (!user) return res.status(400).end();
-
-  const arePasswordEqual = await compare(req.body.password, user.password);
-
-  if (arePasswordEqual) {
-    const tinyUser: any = {
-      name: user.name,
-      surname: user.surname,
-      email: user.email,
-    };
-
-    return res.status(200).json(tinyUser);
-  }
-
-  res.status(401).end();
 };
 
 export default credentialsAuth;
--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -3,30 +3,34 @@ import { registerSchema } from "lib/schemas";
 import { hash } from "bcrypt";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return req.status(405).end();
+  try {
+    if (req.method !== "POST") {
+      return req.status(405).end();
+    }
+
+    const userInfo = JSON.parse(req.body);
+
+    if (!(await registerSchema.isValid(req.body))) return res.status(400).end();
+
+    const userAlreadyExist = await prisma.user.findFirst({
+      where: { email: userInfo.email },
+    });
+
+    if (userAlreadyExist) return res.status(409).end();
+
+    const encryptedPassword = await hash(userInfo.password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name: userInfo.name,
+        surname: userInfo.surname,
+        email: userInfo.email,
+        password: encryptedPassword,
+      },
+    });
+
+    return res.status(201).json({ user });
+  } catch (err) {
+    res.status(500).json({ error: "failed to load data" });
   }
-
-  const userInfo = JSON.parse(req.body);
-
-  if (!(await registerSchema.isValid(req.body))) return res.status(400).end();
-
-  const userAlreadyExist = await prisma.user.findFirst({
-    where: { email: userInfo.email },
-  });
-
-  if (userAlreadyExist) return res.status(409).end();
-
-  const encryptedPassword = await hash(userInfo.password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      name: userInfo.name,
-      surname: userInfo.surname,
-      email: userInfo.email,
-      password: encryptedPassword,
-    },
-  });
-
-  return res.status(201).json({ user });
 }
